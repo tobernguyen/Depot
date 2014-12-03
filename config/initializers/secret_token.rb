32a0133146425f19172a7e8e6b4ab7e24774474b
East Agile RoR <ea@Longs-MacBook-Pro.local>
2014-12-03 22:51:59 +0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '97f3bf456cfddc8f6ed6f8aa98bf97b92c5335c88eadfc13db0303422b2c1995f61f80ef43c8116d59b244189ec33f4436f1c162be8a05d30d410b90c502002d'
