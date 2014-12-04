0977d9ef6ce81fb1a241ab404a38b47ab2cd11fe
Long Nguyen <long.nguyen@eastagile.com>
2014-12-04 16:33:55 +0700

    [f] shoping cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  
+
   def index
     @products = Product.order(:title)
+    @count = increment_count
   end
+
+  private
+
+    def increment_count
+    	session[:count] ||= 0
+    	session[:count] += 1
+    end
 end
