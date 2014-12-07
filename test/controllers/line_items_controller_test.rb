4c073bb7cc61c0bc893aeb1b1d8aa981737a6ac2
Long Nguyen <long.nguyen@eastagile.com>
2014-12-07 14:21:10 +0700

    [f] ajax the shopping cart + refactor the test

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -16,14 +16,6 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create line_item" do
-    assert_difference('LineItem.count') do
-      post :create, product_id: products(:ruby).id
-    end
-
-    assert_redirected_to cart_path(assigns(:line_item).cart)
-  end
-
   test "should show line_item" do
     get :show, id: @line_item
     assert_response :success
@@ -45,5 +37,16 @@ class LineItemsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to line_items_path
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end
+
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
   end
 end
